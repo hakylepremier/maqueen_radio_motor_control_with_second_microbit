ae28a684d74d93e3073621232a0be39ae646d3c2
Humphrey Yeboah <haky@haky.com>
2024-04-13 14:54:56 +0000

    feat: add microbit accelerometer control
    The accelerometer values will change depending on how you tilt the microbit. This will be used to control the robot. This commit will add a simple way to see the direction of microbit when tilted and this direction will be used later on to control the robot.

---
+++ microbit_controller.py
@@ -0,0 +1,27 @@
+from microbit import *
+
+accel_y = 0
+accel_x = 0
+# This changes the sensitivity in the x direction (left, right)
+accel_x_offset = 400
+# This changes the sensitivity in the y direction (forward, backward)
+accel_y_offset = 400
+direction = "F"
+display.show(Image.HAPPY)
+
+while True:
+    accel_x = accelerometer.get_x()
+    accel_y = accelerometer.get_y()
+
+    if accel_x > accel_x_offset:
+        direction = "R"
+    elif accel_x < accel_x_offset * -1:
+        direction = "L"
+    if accel_y > accel_y_offset:
+        direction = "F"
+    elif accel_y < accel_y_offset * -1:
+        direction = "B"
+    if accel_x < accel_x_offset and accel_x > accel_x_offset * -1 and (accel_y < accel_y_offset and accel_y > accel_y_offset * -1):
+        direction = "-"
+    display.show(direction)
+    sleep(50)
